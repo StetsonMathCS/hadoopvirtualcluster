366e4891319b5a8978f41a048bf989d521ff7029
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2015-02-21 17:38:26 -0500

    Disable munin (too slow).

--- setup-hadoop.sh
+++ setup-hadoop.sh
@@ -21,23 +21,34 @@ function updatemunin() {
 	echo >> /etc/munin/conf.d/hadoop.conf
 }
 
-echo "Erasing /etc/munin/conf.d/hadoop.conf"
-echo > /etc/munin/conf.d/hadoop.conf
+# args: hostname ip
+function updatehosts() {
+	echo "$2 $1" >> ./hosts
+}
+
+#echo "Erasing /etc/munin/conf.d/hadoop.conf"
+#echo > /etc/munin/conf.d/hadoop.conf
+
+echo "Erasing ./hosts"
+echo "127.0.0.1 localhost" > ./hosts
 
 getip "namenode"
 NAMENODEIP=$IP
 echo "namenode: $NAMENODEIP"
-updatemunin "namenode" $NAMENODEIP
+#updatemunin "namenode" $NAMENODEIP
+updatehosts "namenode" $NAMENODEIP
 
 getip "resourcemanager"
 RESOURCEMANAGERIP=$IP
 echo "resourcemanager: $RESOURCEMANAGERIP"
-updatemunin "resourcemanager" $RESOURCEMANAGERIP
+#updatemunin "resourcemanager" $RESOURCEMANAGERIP
+updatehosts "resourcemanager" $RESOURCEMANAGERIP
 
 getip "mrjobhistory"
 MRJOBHISTORYIP=$IP
 echo "mrjobhistory: $MRJOBHISTORYIP"
-updatemunin "mrjobhistory" $MRJOBHISTORYIP
+#updatemunin "mrjobhistory" $MRJOBHISTORYIP
+updatehosts "mrjobhistory" $MRJOBHISTORYIP
 
 for f in `ls local-hadoop-etc`;
 do
@@ -49,6 +60,15 @@ do
         sed "s!___DATADIR___!$DATADIR!g" \
         > /opt/hadoop-2.6.0/etc/hadoop/$f
 done
+
+for j in `vagrant status | grep -oE '(slave[0-9]+)'`
+do
+  getip $j
+  #updatemunin $j $IP
+  updatehosts $j $IP
+done
+
+vagrant reload
 
 echo "Restarting namenode daemon..."
 vagrant ssh namenode -- "sudo /vagrant/vm-update-hadoop-ips.sh 0.0.0.0 $RESOURCEMANAGERIP $MRJOBHISTORYIP /usr/lib/jvm/jdk1.7.0_71 /home/vagrant/logs /home/vagrant/data"
@@ -68,8 +88,6 @@ vagrant ssh mrjobhistory -- "sudo /home/vagrant/hadoop/hadoop-2.6.0/sbin/mr-jobh
 
 for j in `vagrant status | grep -oE '(slave[0-9]+)'`
 do
-  getip $j
-  updatemunin $j $IP
   echo "Restarting datanode and nodemanager daemons on $j"
   vagrant ssh $j -- "sudo /vagrant/vm-update-hadoop-ips.sh $NAMENODEIP $RESOURCEMANAGERIP $MRJOBHISTORYIP /usr/lib/jvm/jdk1.7.0_71 /home/vagrant/logs /home/vagrant/data"
   vagrant ssh $j -- "sudo /home/vagrant/hadoop/hadoop-2.6.0/sbin/hadoop-daemon.sh --config /home/vagrant/hadoop/hadoop-2.6.0/etc/hadoop/ --script hdfs stop datanode"
@@ -78,5 +96,7 @@ do
   vagrant ssh $j -- "sudo /home/vagrant/hadoop/hadoop-2.6.0/sbin/yarn-daemon.sh --config /home/vagrant/hadoop/hadoop-2.6.0/etc/hadoop/ start nodemanager"
 done
 
-sudo /sbin/service munin-node restart
+
+#sudo /sbin/service munin-node restart
+
 
